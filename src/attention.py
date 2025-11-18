17bf2535cf59d93ca14640ce344df3ed71f740df
daiyada <shunsuke07l6@gmail.com>
2025-11-18 17:18:52 +0900

    add: multi head attention

--- src/attention.py
+++ src/attention.py
@@ -46,30 +46,68 @@ class CausalAttention(nn.Module):
         return context_vec
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--d_out", default=2)
-    args = parser.parse_args()
+class MultiHeadAttention(nn.Module):
 
-    inputs = torch.tensor(
-        [
-            [0.43, 0.15, 0.89],  # Your
-            [0.55, 0.87, 0.66],  # journey
-            [0.57, 0.85, 0.64],  # starts
-            [0.22, 0.58, 0.33],  # with
-            [0.77, 0.25, 0.10],  # one
-            [0.05, 0.80, 0.55],  # step
-        ]
-    )
-    batch = torch.stack([inputs, inputs], dim=0)
-    d_in = inputs.shape[1]
-    context_length = batch.shape[1]
-    ca = CausalAttention(
-        d_in=d_in,
-        d_out=args.d_out,
-        context_length=context_length,
-        dropout=0.0,
-        qkv_bias=False,
-    )
-    context_vecs = ca(batch)
-    print(f"context_vecs.shape: {context_vecs.shape}")
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        context_length: int,
+        dropout: float,
+        num_heads: int,
+        qkv_bias: bool = False,
+    ):
+        """Define constructor."""
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self._d_out = d_out
+        self._num_heads = num_heads
+        self._head_dim = d_out // num_heads
+
+        self._W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self._W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self._W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+        # Combine output of head by using linear layer
+        self._out_proj = nn.Linear(d_out, d_out)
+        self._dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(
+                torch.ones(context_length, context_length), diagonal=1
+            )
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Define forward propagation."""
+        b, num_tokens, d_in = x.shape
+
+        queries = self._W_query(x)
+        keys = self._W_key(x)
+        values = self._W_value(x)
+
+        queries = queries.view(b, num_tokens, self._num_heads, self._head_dim)
+        keys = keys.view(b, num_tokens, self._num_heads, self._head_dim)
+        values = values.view(b, num_tokens, self._num_heads, self._head_dim)
+
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-2] ** 0.5, dim=-1)
+        attn_weights = self._dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(
+            b, num_tokens, self._d_out
+        )
+        context_vec = self._out_proj(context_vec)
+
+        return context_vec
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,6 +1,7 @@
+import pytest
 import torch
 
-from attention import CausalAttention
+from attention import CausalAttention, MultiHeadAttention
 
 
 def test_causal_attn(batch_tensor: torch.Tensor) -> None:
@@ -18,3 +19,43 @@ def test_causal_attn(batch_tensor: torch.Tensor) -> None:
     assert context_vecs.shape == torch.Size(
         [batch_size, context_length, d_out]
     )
+
+def test_positive_multi_head_attn(batch_tensor: torch.Tensor) -> None:
+    """Test multi head attention object in case of positive."""
+    d_out = 2
+    batch_size, context_length, d_in = batch_tensor.shape
+    mha = MultiHeadAttention(
+        d_in=d_in,
+        d_out=d_out,
+        context_length=context_length,
+        dropout=0.,
+        num_heads=2,
+        qkv_bias=False
+    )
+    context_vecs = mha(batch_tensor)
+    assert context_vecs.shape == torch.Size(
+        [batch_size, context_length, d_out]
+    )
+
+@pytest.mark.parametrize(
+        "d_out, num_heads",
+        [
+            (3, 2),
+            (2, 3),
+        ]
+)
+def test_negative_multi_head_attn(
+    batch_tensor: torch.Tensor, d_out: int, num_heads: int
+) -> None:
+    """Test multi head attention object in case of negative."""
+    _, context_length, d_in = batch_tensor.shape
+    with pytest.raises(AssertionError) as e:
+        mha = MultiHeadAttention(
+            d_in=d_in,
+            d_out=d_out,
+            context_length=context_length,
+            dropout=0.,
+            num_heads=num_heads,
+            qkv_bias=False
+        )
+    assert str(e.value) == "d_out must be divisible by num_heads"
