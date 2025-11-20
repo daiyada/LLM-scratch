c2a721cdf5e2b9905db05e09494c1500efaa14da
daiyada <shunsuke07l6@gmail.com>
2025-11-20 18:49:54 +0900

    add: loguru

--- src/LLM_scratch/attention.py
+++ src/LLM_scratch/attention.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from loguru import logger
 
 
 class CausalAttention(nn.Module):
@@ -57,6 +58,18 @@ class MultiHeadAttention(nn.Module):
     ):
         """Define constructor."""
         super().__init__()
+        logger.info(
+            f"""\n
+            \t###################################
+            \t  d_in:\t\t\t{d_in}
+            \t  d_out:\t\t{d_out}
+            \t  context_length:\t{context_length}
+            \t  dropout:\t\t{dropout}
+            \t  num_heads:\t\t{num_heads}
+            \t  qkv_bias:\t\t{qkv_bias}
+            \t###################################
+            """
+        )
         assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
 
         self._d_out = d_out
@@ -77,15 +90,17 @@ class MultiHeadAttention(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Define forward propagation."""
-        b, num_tokens, d_in = x.shape
+        b, num_tokens, _ = x.shape
 
         queries = self._W_query(x)
         keys = self._W_key(x)
         values = self._W_value(x)
+        print(queries)
 
         queries = queries.view(b, num_tokens, self._num_heads, self._head_dim)
         keys = keys.view(b, num_tokens, self._num_heads, self._head_dim)
         values = values.view(b, num_tokens, self._num_heads, self._head_dim)
+        print(queries)
 
         queries = queries.transpose(1, 2)
         keys = keys.transpose(1, 2)
@@ -96,14 +111,12 @@ class MultiHeadAttention(nn.Module):
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
 
         attn_scores.masked_fill_(mask_bool, -torch.inf)
-
         attn_weights = torch.softmax(
-            attn_scores / keys.shape[-2] ** 0.5, dim=-1
+            attn_scores / keys.shape[-1] ** 0.5, dim=-1
         )
         attn_weights = self._dropout(attn_weights)
 
         context_vec = (attn_weights @ values).transpose(1, 2)
         context_vec = context_vec.contiguous().view(b, num_tokens, self._d_out)
         context_vec = self._out_proj(context_vec)
-
         return context_vec
