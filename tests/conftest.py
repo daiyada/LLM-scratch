90c532e0a130569bcbb9e1b5e4f9f403a7ceec4b
daiyada <shunsuke07l6@gmail.com>
2025-11-21 11:34:50 +0900

    update: test_layer_norm

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,11 +23,3 @@ def batch_tensor(inputs_tensor: torch.Tensor) -> torch.Tensor:
     """Example of tensor batch consisted of 2 inputs_tensors."""
     batch = torch.stack([inputs_tensor, inputs_tensor], dim=0)
     return batch
-
-
-@pytest.fixture(scope="function")
-def batch_example() -> torch.Tensor:
-    """Define batch example."""
-    torch.manual_seed(123)
-    batch_example = torch.randn(2, 5)
-    return batch_example
--- tests/test_layer_norm.py
+++ tests/test_layer_norm.py
@@ -3,16 +3,20 @@ import torch
 from LLM_scratch.layer_norm import LayerNorm
 
 
-def test_layer_norm(batch_example: torch.Tensor) -> None:
+def test_layer_norm(batch_tensor: torch.Tensor) -> None:
     """Test layer norm object."""
-    emb_dim = 5
+    batch, num_tokens, emb_dim = batch_tensor.shape
     ln = LayerNorm(emb_dim=emb_dim)
-    out_ln = ln(batch_example)
+    out_ln = ln(batch_tensor)
     mean = out_ln.mean(dim=-1, keepdim=True)
     var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
     assert torch.allclose(
-        mean.detach(), torch.tensor([[-2.9802e-08], [0.0000e00]]), atol=1e-4
+        mean.detach(),
+        torch.zeros(batch, num_tokens, 1),
+        atol=1e-4,
     )
     assert torch.allclose(
-        var.detach(), torch.tensor([[1.0000], [1.0000]]), atol=1e-4
+        var.detach(),
+        torch.ones(batch, num_tokens, 1),
+        atol=1e-3,
     )
